cc19bf668b809ef07eb38cc39e762b3deba2af65
Samarth K <97102757+samarthk2402@users.noreply.github.com>
2026-09-05 11:19:20 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def matchAnagram(string, anagram):
+        if len(string) != len(anagram):
+            return False
+
+        for char in string:
+            if not char in anagram:
+                return False
+
+        return True
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # use a hash map to store previous anagrams
+        anagram_to_strings = {}
+
+        # for each new string check if in anagrams
+        for string in strs:
+            sorted_string = "".join(sorted(string))
+            if sorted_string in anagram_to_strings.keys():
+                anagram_to_strings[sorted_string].append(string)
+            else:
+                anagram_to_strings[sorted_string] = [string]
+
+        return list(anagram_to_strings.values())
+            
